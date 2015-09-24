b436c94adb217d5a42dcd0d2c040290615b67834
alexcleu <alexcleu@gmail.com>
2015-09-23 19:21:00 -0700

    debugged: fixed the tournament.sql script Removed the unnecessary comma and dropped the database whenever it is activated.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -8,19 +8,31 @@
 
 
 /* SCHEMA */ 
+--Drop the database if it exsits
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
 
+--Create table for players
+--id: specific id for players
+--name: player's name
 CREATE TABLE PLAYERS (
 	ID serial PRIMARY KEY,
-	NAME TEXT,
+	NAME TEXT
 );
 
+--Create table for matches
+--id: specific id for matches
+--wins: count how many wins, starting from 0
+--matches: report how many mathces, starting from 0
+--player_id: record the player's status
 CREATE TABLE MATCHES(
 	ID serial primary key,
 	wins int DEFAULT 0,
 	matches int DEFAULT 0,
-	player_id references players(id),
+	player_id int references players(id)
 );
-					 
+--Create view to pair two players up				 
 CREATE VIEW PAIRING as
     SELECT a.player_id as id1, 
            b.player_id as id2
@@ -29,7 +41,8 @@ CREATE VIEW PAIRING as
     AND a.player_id != b.player_id
     and a.player_id > b.player_id
 ;
-
+--Create a table to record games players had won with out playing
+--player_id: player_id of the player that receives a free win
 CREATE TABLE FREEWIN(
   player_id int references players(id)
 );
